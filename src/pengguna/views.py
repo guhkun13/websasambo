6bdaf69226b2258c98daa5467baa04c0a610976b
guhkun13 <guhkun13@gmail.com>
2021-08-29 02:31:08 +0800

    edit and save

--- src/pengguna/views.py
+++ src/pengguna/views.py
@@ -35,8 +35,13 @@ def add(request):
     html = 'pengguna/add.html'
     return render (request, html,context) 
 
-def edit(request):
-    pass 
+def edit(request, id):
+    context = {}
+
+    context['obj'] = Pengguna.objects.get(id=id)
+
+    html = 'pengguna/edit.html'
+    return render (request, html,context)  
 
 def save(request):
     func_name = "save"
@@ -44,26 +49,44 @@ def save(request):
 
     print("THIS IS SAVE FUNC @" + app_name) 
     if request.method == 'POST':
+         
         _POST = request.POST
-        try:
-            data = Pengguna()
-            data.nama_lengkap = _POST.get('nama_lengkap')
-            data.nama_panggilan = _POST.get('nama_panggilan')
-            data.tanggal_lahir = _POST.get('tanggal_lahir')
-            data.user = get_user_by_username(_POST.get('username'))
-            data.save()
 
-        except Exception as e:
-            print ("exce on @" + app_name + "/" + func_name)
-            print (e)
+        if (_POST.get('pk')) :
+            # means update
+            try:
+                data = Pengguna.objects.get(pk=_POST.get('pk'))
+                data.jenis_kelamin = _POST.get('jenis_kelamin')
+                data.save()
+            except Exception as e:
+                print ("exce on @" + app_name + "/" + func_name)
+                print (e)
 
-            msg = str(e)
-            messages.add_message(request, _ERROR, msg)
-            return HttpResponseRedirect(reverse('pengguna:add'))
+                msg = str(e)
+                messages.add_message(request, _ERROR, msg)
+                return HttpResponseRedirect(reverse('pengguna:edit'))    
+        else:
+            try:
+                data = Pengguna()
+                data.nama_lengkap = _POST.get('nama_lengkap')
+                data.nama_panggilan = _POST.get('nama_panggilan')
+                data.tanggal_lahir = _POST.get('tanggal_lahir')
+                data.jenis_kelamin = _POST.get('jenis_kelamin')
+                data.user = get_user_by_username(_POST.get('username'))
+                data.save()
 
-        msg = 'berhasil assign pengguna'
-        messages.add_message(request, _SUCCESS, msg)
-        return HttpResponseRedirect(reverse('pengguna:index'))
+            except Exception as e:
+                print ("exce on @" + app_name + "/" + func_name)
+                print (e)
+
+                msg = str(e)
+                messages.add_message(request, _ERROR, msg)
+                return HttpResponseRedirect(reverse('pengguna:add'))
+
+            msg = 'berhasil assign pengguna'
+            messages.add_message(request, _SUCCESS, msg)
+            return HttpResponseRedirect(reverse('pengguna:index'))
+    
         
     return HttpResponseRedirect(reverse('pengguna:index'))
     
