98db8bb4a17c3303688da5240684abb354ed762c
guhkun13 <guhkun13@gmail.com>
2021-09-08 09:43:28 +0800

    CRUD pengguna

--- src/pengguna/views.py
+++ src/pengguna/views.py
@@ -5,9 +5,11 @@ from django.urls import reverse
 from django.contrib.auth.decorators import  login_required
 from django.contrib import auth, messages
 
-from .models import Pengguna
+from mainapp.models import JenjangPendidikan
 from mainapp.utils import get_user_by_username
 # Create your views here.
+
+from pengguna.models import Pengguna
 
 
 _DEBUG	     = 10
@@ -19,6 +21,7 @@ _ERROR	     = 40
 
 app_name = 'pengguna'
 
+@login_required
 def index(request):
     context = {}
     pengguna = Pengguna.objects.get(user=request.user)
@@ -28,16 +31,20 @@ def index(request):
     html = app_name + '/index.html'
     return render (request, html, context) 
 
+@login_required
 def add(request):
     context = {}
+    context['jenjang_pendidikans'] = JenjangPendidikan.objects.all()
 
     html = app_name +  '/add.html'
     return render (request, html,context) 
 
+@login_required
 def edit(request, id):
     context = {}
 
     context['obj'] = Pengguna.objects.get(id=id)
+    context['jenjang_pendidikans'] = JenjangPendidikan.objects.all()
 
     html = app_name +  '/edit.html'
     return render (request, html,context)  
@@ -51,13 +58,17 @@ def impl_save_pengguna(request, data, _POST, tipe):
     data.jenis_kelamin = _POST.get('jenis_kelamin')
     data.tempat_lahir = _POST.get('tempat_lahir')
     data.tanggal_lahir = _POST.get('tanggal_lahir')
+    ijazah_tertinggi = JenjangPendidikan.objects.get(pk= _POST.get('ijazah_tertinggi'))
+    data.fk_ijazah_tertinggi = ijazah_tertinggi
 
     data.nomor_hp = _POST.get('nomor_hp')
     data.email = _POST.get('email')
     if (tipe == 'new'):
         data.user = get_user_by_username(_POST.get('username'))
+
     return data
 
+@login_required
 def save(request):
     func_name = "save"
     context = {}
@@ -67,8 +78,10 @@ def save(request):
          
         _POST = request.POST
 
+
         if (_POST.get('pk')) :
             # means update
+            pk = _POST.get('pk')
             try:
                 data = Pengguna.objects.get(pk=_POST.get('pk'))
                 data = impl_save_pengguna(request, data, _POST, 'update')
@@ -80,7 +93,7 @@ def save(request):
 
                 msg = str(e)
                 messages.add_message(request, _ERROR, msg)
-                return HttpResponseRedirect(reverse('pengguna:edit'))    
+                return HttpResponseRedirect(reverse('pengguna:edit', args=[pk]))    
         else:
             try:
                 data = Pengguna()
@@ -104,5 +117,6 @@ def save(request):
     
 
 
+@login_required
 def delete(request, id):
     pass 
